75c82adc969fe1c91eabadba9fc7a5a01da71760
张志强 <zhiqiang@bianjie.ai>
2020-04-22 15:56:11 +0800

    update docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -52,6 +52,7 @@ module.exports = {
         locales: {
             "/": {
                 sidebar: sidebar("", [
+                    ["安装", "/installation.md"],
                     ["功能模块", "/features"],
                     ["命令行客户端", "/cli-client"],
                     ["API 服务", "/light-client"],
