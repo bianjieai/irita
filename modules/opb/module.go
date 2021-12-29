f6a74cbe66d216b6be4197472bd2deb962700efa
sheldon <997166273@qq.com>
2021-12-29 19:45:55 +0800

    fix: opb add migrations

--- modules/opb/module.go
+++ modules/opb/module.go
@@ -106,6 +106,8 @@ func (AppModule) Name() string {
 func (am AppModule) RegisterServices(cfg module.Configurator) {
 	types.RegisterMsgServer(cfg.MsgServer(), keeper.NewMsgServerImpl(am.keeper))
 	types.RegisterQueryServer(cfg.QueryServer(), am.keeper)
+	m := keeper.NewMigrator(am.keeper)
+	cfg.RegisterMigration(types.ModuleName, 1, m.Migrate1to2)
 }
 
 // RegisterInvariants registers the OPB module invariants.
