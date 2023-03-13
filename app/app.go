9621ac44f0c735be91ba2eaf42fddb17a1d3e8d1
aofengli <996177263@qq.com>
2023-03-13 17:41:34 +0800

    fix v3.3.0-wenchangchain-tianzhou upgrade plan

--- app/app.go
+++ app/app.go
@@ -6,6 +6,10 @@ import (
 	"os"
 	"path/filepath"
 
+	"github.com/cosmos/cosmos-sdk/x/staking"
+
+	"github.com/cosmos/cosmos-sdk/x/capability"
+
 	"github.com/irisnet/irismod/modules/mt"
 
 	"github.com/CosmWasm/wasmd/x/wasm"
@@ -732,6 +736,30 @@ func NewIritaApp(
 	app.RegisterUpgradePlan(
 		"v3.3.0-wenchangchain-tianzhou", store.StoreUpgrades{},
 		func(ctx sdk.Context, plan sdkupgrade.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+
+			fromVM[authtypes.ModuleName] = auth.AppModule{}.ConsensusVersion()
+			fromVM[banktypes.ModuleName] = bank.AppModule{}.ConsensusVersion()
+			fromVM[stakingtypes.ModuleName] = staking.AppModule{}.ConsensusVersion()
+			fromVM[opbtypes.ModuleName] = opb.AppModule{}.ConsensusVersion()
+			fromVM[identitytypes.ModuleName] = identity.AppModule{}.ConsensusVersion()
+			fromVM[cslashing.ModuleName] = cslashing.AppModule{}.ConsensusVersion()
+			fromVM[capabilitytypes.ModuleName] = capability.AppModule{}.ConsensusVersion()
+			fromVM[nodetypes.ModuleName] = node.AppModule{}.ConsensusVersion()
+			fromVM[genutiltypes.ModuleName] = genutil.AppModule{}.ConsensusVersion()
+			fromVM[paramstypes.ModuleName] = cparams.AppModule{}.ConsensusVersion()
+			fromVM[crisistypes.ModuleName] = crisis.AppModule{}.ConsensusVersion()
+			fromVM[upgradetypes.ModuleName] = crisis.AppModule{}.ConsensusVersion()
+			fromVM[evidencetypes.ModuleName] = evidence.AppModule{}.ConsensusVersion()
+			fromVM[feegrant.ModuleName] = feegrantmodule.AppModule{}.ConsensusVersion()
+			fromVM[tokentypes.ModuleName] = token.AppModule{}.ConsensusVersion()
+			fromVM[recordtypes.ModuleName] = record.AppModule{}.ConsensusVersion()
+			fromVM[nfttypes.ModuleName] = nft.AppModule{}.ConsensusVersion()
+			fromVM[servicetypes.ModuleName] = service.AppModule{}.ConsensusVersion()
+			fromVM[oracletypes.ModuleName] = oracle.AppModule{}.ConsensusVersion()
+			fromVM[randomtypes.ModuleName] = random.AppModule{}.ConsensusVersion()
+			fromVM[permtypes.ModuleName] = perm.AppModule{}.ConsensusVersion()
+			fromVM[feemarkettypes.ModuleName] = feemarket.AppModule{}.ConsensusVersion()
+			fromVM[evmtypes.ModuleName] = evm.AppModule{}.ConsensusVersion()
 
 			return app.mm.RunMigrations(ctx, app.configurator, fromVM)
 		},
